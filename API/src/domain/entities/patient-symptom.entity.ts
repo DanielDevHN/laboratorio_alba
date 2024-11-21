b4be4837058b69442efaac028af0589515a43906
DanielDevHN <dangrereyes@gmail.com>
2024-11-21 01:14:04 -0600

    Add patient-symptom entity
    Added an entity to relate patient and symptom data.

---
+++ API/src/domain/entities/patient-symptom.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, ObjectIdColumn, Column } from "typeorm";
+
+@Entity()
+export class PatientSymptom {
+  @ObjectIdColumn()
+  id!: string;
+
+  @Column()
+  patientId!: string;
+
+  @Column()
+  symptomId!: string;
+
+  @Column()
+  assignedBy!: string;
+
+  @Column()
+  assignedAt!: Date;
+}
