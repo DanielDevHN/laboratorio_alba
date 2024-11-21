a1c38d53eb6e6d00c7315b11fd5f4fcd6ca8db87
DanielDevHN <dangrereyes@gmail.com>
2024-11-21 16:01:07 -0600

    Update symptom controller
    Added other constants to consume the service.

--- API/src/interfaces/controllers/symptom.controller.ts
+++ API/src/interfaces/controllers/symptom.controller.ts
@@ -7,3 +7,13 @@ export const createSymptom = async (req: Request, res: Response) => {
     const symptom = await symptomService.createSymptom(req.body);
     res.status(201).json(symptom);
 }
+
+export const getSymptoms = async (req: Request, res: Response) => {
+    const symptoms = await symptomService.findAllSymptoms();
+    res.status(200).json(symptoms);
+}
+
+export const deletedSymptom = async (req: Request, res: Response) => {
+    await symptomService.deleteSymptom(req.params.id);
+    res.status(200).json({ message: 'Symptom deleted successfully' });
+}
