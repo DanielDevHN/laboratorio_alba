8ec1e184782165c2d104326929b3f50ed18a4d61
DanielDevHN <dangrereyes@gmail.com>
2024-11-21 18:41:34 -0600

    Add symptom usecase

---
+++ API/src/application/usecases/symptom.usecase.ts
@@ -0,0 +1,22 @@
+import { SymptomRepository } from "../../domain/repositories/symptom.repository";
+import { Symptom } from "../../domain/entities/symptom.entity";
+
+export class SymptomUseCase {
+  constructor(private readonly symptomRepository: SymptomRepository) {}
+
+  async createSymptom(symptomData: Partial<Symptom>): Promise<Symptom> {
+    return await this.symptomRepository.createSymptom(symptomData);
+  }
+
+  async findAllPatients(): Promise<Symptom[]> {
+    return await this.symptomRepository.findAllSymptoms();
+  }
+
+  async findSymptomById(symptomId: string): Promise<Symptom | null> {
+    return await this.symptomRepository.findSymptomById(symptomId);
+  }
+
+  async deleteSymptom(symptomId: string): Promise<void> {
+    return await this.symptomRepository.deleteSymptom(symptomId);
+  }
+}
