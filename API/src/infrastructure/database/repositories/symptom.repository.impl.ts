be5120adabd756d8b0164267f0e7a17bdd6bc410
DanielDevHN <dangrereyes@gmail.com>
2024-11-21 18:30:56 -0600

    Add symptom repository implementation

---
+++ API/src/infrastructure/database/repositories/symptom.repository.impl.ts
@@ -0,0 +1,25 @@
+import { Repository } from "typeorm";
+import { Symptom } from "../entities/symptom.entity";
+import { AppDataSource } from "../../env/config";
+import { SymptomRepository } from "../../../domain/repositories/symptom.repository";
+
+export class SymptomRepositoryImpl implements SymptomRepository {
+    private repository: Repository<Symptom> = AppDataSource.getRepository(Symptom);
+
+    async createSymptom(symptom: Partial<Symptom>): Promise<Symptom> {
+        return await this.repository.save(symptom);
+    }
+
+    async findAllPatients(): Promise<Symptom[]> {
+        return await this.repository.find();
+    }
+
+    async findSymptomById(symptomId: string): Promise<Symptom | null> {
+        return await this.repository.findOneBy({ id: symptomId });
+    }
+
+    async deletePatient(symptomId: string): Promise<void> {
+        await this.repository.delete({ id: symptomId });
+    }
+
+}
--- API/src/infrastructure/database/repositories/symptom.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Repository } from "typeorm";
-import { Symptom } from "../entities/symptom.entity";
-import { AppDataSource } from "../../env/config";
-
-export const SymptomRepository = AppDataSource.getRepository(Symptom);
