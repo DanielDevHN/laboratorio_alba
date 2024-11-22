2d6b26cd7159eec8aaa5e3dee487043f3a1c566a
DanielDevHN <dangrereyes@gmail.com>
2024-11-21 19:06:56 -0600

    Update patient-symptom controller

--- API/src/interfaces/controllers/patient-symptom.controller.ts
+++ API/src/interfaces/controllers/patient-symptom.controller.ts
@@ -1,22 +1,36 @@
 import { Request, Response } from "express";
-import { PatientSymptomService } from "../../application/services/patient-symptom.service";
+import { PatientSymptomUseCase } from "../../application/usecases/patient-symptom.usecase";
+import { PatientSymptomRepositoryImpl } from "../../infrastructure/database/repositories/patient-symptom.repository.impl";
 
-const patientSymptomService = new PatientSymptomService();
+const patientSymptomRepository = new PatientSymptomRepositoryImpl();
+const patientSymptomUseCase = new PatientSymptomUseCase(patientSymptomRepository);
 
 export const assignSymptom = async (req: Request, res: Response) => {
-    const { patientId, symptomId } = req.params;
-    const relation = await patientSymptomService.assingSymptomToPatient(patientId, symptomId);
-    res.status(201).json(relation);
+    try {
+        const { patientId, symptomId } = req.params;
+        const relation = await patientSymptomUseCase.assignSymptomToPatient(patientId, symptomId);
+        res.status(201).json(relation);
+    } catch (error) {
+        res.status(400).json({ message: error instanceof Error ? error.message : "Unexpected error" }); 
+    }
 }
 
-export const getSymptomsForPatient = async (req: Request, res: Response) => {
-    const { patientId } = req.params;
-    const symptoms = await patientSymptomService.getSymptomsForPatient(patientId);
-    res.status(200).json(symptoms);
+export const findSymptomsByPatientId = async (req: Request, res: Response) => {
+    try {
+        const { patientId } = req.params;
+        const symptoms = await patientSymptomUseCase.findSymptomsByPatientId(patientId);
+        res.status(200).json(symptoms);
+    } catch (error) {
+        res.status(400).json({ message: error instanceof Error ? error.message : "Unexpected error" });
+    }
 }
 
-export const removeSymptom = async (req: Request, res: Response) => {
-    const { patientId, symptomId } = req.params;
-    await patientSymptomService.removeSymptomFromPatient(patientId, symptomId);
-    res.status(204).json({ message: 'Symptom removed from patient' });
+export const removeSymptomFromPatient = async (req: Request, res: Response) => {
+    try {
+        const { patientId, symptomId } = req.params;
+        await patientSymptomUseCase.removeSymptomFromPatient(patientId, symptomId);
+        res.status(204).json({ message: 'Symptom removed from patient' });
+    } catch (error) {
+        res.status(400).json({ message: error instanceof Error ? error.message : "Unexpected error" });
+    }
 }
