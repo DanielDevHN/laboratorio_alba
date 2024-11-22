02c3b174d2c38ed99d2a8e507b299c1b88bd9cb3
DanielDevHN <dangrereyes@gmail.com>
2024-11-21 18:29:56 -0600

    Update user controller

--- API/src/interfaces/controllers/user.controller.ts
+++ API/src/interfaces/controllers/user.controller.ts
@@ -1,26 +1,49 @@
 import { Request, Response } from "express";
-import { UserService } from "../../application/services/user.service";
-import jwt from "jsonwebtoken";
+import { UserUseCase } from "../../application/usecases/user.usecase";
+import { UserRepositoryImpl } from "../../infrastructure/database/repositories/user.repository.impl";
 
-const userService = new UserService();
+const userRepository = new UserRepositoryImpl();
+const userUseCase = new UserUseCase(userRepository);
 
 export const createUser = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    const user = await userService.createUser(username, password);
-    res.status(201).json(user);
+    try {
+        const { username, password } = req.body;
+        const user = await userUseCase.createUser(username, password);
+        res.status(201).json(user);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ message: error.message });
+        } else {
+            res.status(400).json({ message: "An unknown error occurred" });
+        }
+    }
 }
 
 export const deactivateUser = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    await userService.deactivateUser(id);
-    res.status(204).json({ message: "User deactivated" });
+    try {
+        const { id } = req.params;
+        await userUseCase.deactivateUser(id);
+        res.status(204).json({ message: "User deactivated successfully" });
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ message: error.message });
+        } else {
+            res.status(400).json({ message: "An unknown error occurred" });
+        }
+    }
 }
 
 export const login = async (req: Request, res: Response) => {
-    const { username, password} = req.body;
-    const user = await userService.authenticateUser(username, password);1
-    const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET!, {
-        expiresIn: "1h"
-    });
-    res.status(200).json({ token });
+    try {
+        const { username, password} = req.body;
+        const token = await userUseCase.authenticateUser(username, password);
+        res.status(200).json({ token });
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ message: error.message });
+        } else {
+            res.status(400).json({ message: "An unknown error occurred" });
+        }
+        
+    }
 }
