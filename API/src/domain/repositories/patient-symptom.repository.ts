18570124e48a7d1175e68d04afe5656a18e58e68
DanielDevHN <dangrereyes@gmail.com>
2024-11-22 22:36:20 -0600

    Remove unnecesary methods

--- API/src/domain/repositories/patient-symptom.repository.ts
+++ API/src/domain/repositories/patient-symptom.repository.ts
@@ -3,6 +3,6 @@ import { PatientSymptom } from "../entities/patient-symptom.entity";
 export interface PatientSymptomRepository {
     assignSymptomToPatient(patientId: string, symptomId: string): Promise<PatientSymptom>;
     findSymptomsByPatientId(patientId: string): Promise<PatientSymptom[]>;
-    updateSymptomFromPatient(patientId: string, symptomId: string, patientSymptom: PatientSymptom): Promise<void>;
+    // updateSymptomFromPatient(patientId: string, symptomId: string, patientSymptom: PatientSymptom): Promise<void>;
     removeSymptomFromPatient(patientId: string, symptomId: string): Promise<void>;
 }
--- API/src/domain/repositories/patient.repository.ts
+++ API/src/domain/repositories/patient.repository.ts
@@ -2,8 +2,8 @@ import { Patient } from "../entities/patient.entity";
 
 export interface PatientRepository {
     createPatient(patient: Partial<Patient>): Promise<Patient>;
-    updatePatient(patientId: string, updates: Partial<Patient>): Promise<Patient | null>;
     deletePatient(patientId: string): Promise<void>;
     findAllPatients(): Promise<Patient[]>;
+    updatePatient(patientId: string, updates: Partial<Patient>): Promise<Patient | null>;
     findPatientById(patientId: string): Promise<Patient | null>;
 }
